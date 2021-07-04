6da7c0140c0a5e1c96a7b0260d86eba686796f97
Sunil Jamod <excelenciahub@gmail.com>
2021-07-04 22:38:05 +0530

    Added user CRUD

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,38 +1,82 @@
-exports.findAll = (req, res) => {
+const db = require("../models");
+const User = db.user;
+
+exports.findAll = async (req, res) => {
     try {
-        res.send({ status: true, data: [], message: "Users fetched successfully" })
+        const users = await User.findAll();
+        res.send({ status: true, data: users, message: "Users fetched successfully" })
     } catch (error) {
         res.status(500).send({ status: false, message: error && error.message ? error.message : "Something went wrong" });
     }
 }
 
-exports.findById = (req, res) => {
+exports.findById = async (req, res) => {
     try {
-        res.send({ status: true, data: {}, message: "User fetched successfully" })
+        const { id } = req.params;
+        const user = await User.findByPk(id);
+        res.send({ status: true, data: user, message: "User fetched successfully" })
     } catch (error) {
         res.status(500).send({ status: false, message: error && error.message ? error.message : "Something went wrong" });
     }
 }
 
-exports.add = (req, res) => {
+exports.add = async (req, res) => {
     try {
-        res.send({ status: true, data: req.body, message: "User added successfully" })
+        const { first_name, last_name, description } = req.body;
+
+        if (!first_name || !last_name) {
+            res.status(500).send({ status: false, message: "Some fields are missing from request" });
+        }
+
+        const data = {
+            first_name: first_name,
+            last_name: last_name,
+            description: description
+        }
+
+        const user = await User.create(data);
+        res.send({ status: true, data: user, message: "User added successfully" })
     } catch (error) {
         res.status(500).send({ status: false, message: error && error.message ? error.message : "Something went wrong" });
     }
 }
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
     try {
-        res.send({ status: true, data: {}, message: "User updated successfully" })
+        const { id, first_name, last_name, description } = req.body;
+
+        if (!id, !first_name || !last_name) {
+            res.status(500).send({ status: false, message: "Some fields are missing from request" });
+        }
+
+        const data = {
+            first_name: first_name,
+            last_name: last_name,
+            description: description
+        }
+
+        const user = await User.update(data, { where: { id: id } });
+        console.log(user.length)
+        if (user[0] === 1) {
+            res.send({ status: true, data: {}, message: "User updated successfully" })
+        } else {
+            res.status(500).send({ status: false, message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!` });
+        }
     } catch (error) {
         res.status(500).send({ status: false, message: error && error.message ? error.message : "Something went wrong" });
     }
 }
 
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
     try {
-        res.send({ status: true, data: {}, message: "User deleted successfully" })
+        const { id } = req.params;
+        const user = await User.destroy({ where: { id: id } });
+        console.log(user)
+        if (user == 1) {
+            res.send({ status: true, data: {}, message: "User deleted successfully" })
+        } else {
+            res.status(500).send({ status: false, message: `Cannot delete User with id=${id}. Maybe User was not found or req.body is empty!` });
+        }
     } catch (error) {
         res.status(500).send({ status: false, message: error && error.message ? error.message : "Something went wrong" });
     }
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 require("dotenv").config()
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const routes = require("./routes");
+const db = require("./models");
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3200;
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+db.sequelize.sync({ force: false }).then(() => {
+    console.log("Sync db.");
+});
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
